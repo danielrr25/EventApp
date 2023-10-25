0408fd73cbd1c25c730f4a15fa246969af60caba
oliverkristianfritsche <oliverkristianfritsche@gmail.com>
2023-10-25 01:20:29 -0400

    Initial commit:working login/register api

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,60 @@
 // routes/users.js
 
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const User = require('../models/userSchema');
+
 const router = express.Router();
-const User = require('../models/userSchema'); // Import the User model
 
-// Define your routes and endpoints here
+// Login
+router.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        
+        // Find user by username
+        const user = await User.findOne({ username });
 
-// Example route for creating a user
-router.post('/create', async (req, res) => {
-  try {
-    const user = new User(req.body); // Create a new user from the request body
-    const savedUser = await user.save(); // Save the user to the database
-    res.json(savedUser);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+        if (!user) {
+            return res.status(400).send({ error: 'Invalid login credentials' });
+        }
+
+        // Check the password
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).send({ error: 'Invalid login credentials' });
+        }
+
+        res.send({ message: 'Logged in successfully' });
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
 });
 
-// Other routes (read, update, delete) can be defined similarly
+router.post('/register', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        // Check if username already exists
+        const existingUser = await User.findOne({ username });
+        console.log(req.body);
+        if (existingUser) {
+            return res.status(400).send({ error: 'Username already exists' });
+        }
+
+        // Create a new user
+        const user = new User({ username, password });
+        await user.save();
+
+        res.status(201).send({ message: 'User registered successfully' });
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
 
 module.exports = router;
+
 
 
--- server.js
+++ server.js
@@ -8,22 +8,28 @@ const app = express();
 // Configure middleware
 app.use(bodyParser.json());
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost/eventappdb', {
+// Connect to MongoDB Atlas
+mongoose.connect('mongodb+srv://BigProject:TryG6F2BdhCa14KB@cluster1.uorvble.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('Connected to MongoDB Atlas');
+})
+.catch((err) => {
+  console.error('Error connecting to MongoDB Atlas:', err);
 });
 
 // Import and use your route files here
 const usersRouter = require('./routes/users');
 const eventsRouter = require('./routes/events');
 const attendeesRouter = require('./routes/attendees');
-const chatMessagesRouter = require('./routes/chatMessages');
+const chatMessagesRouter = require('./routes/chatmessages');
 
 app.use('/users', usersRouter);
 app.use('/events', eventsRouter);
 app.use('/attendees', attendeesRouter);
-app.use('/chatMessages', chatMessagesRouter);
+app.use('/chatmessages', chatMessagesRouter);
 
 // Start the Express server
 const port = process.env.PORT || 3000;
