f867dde17188ed2a40c9e731c17f187f2f4cd552
ashawaters <55795969+ashawaters@users.noreply.github.com>
2023-10-26 15:53:07 -0400

    Update Login.hs
    Added handle login information

--- web/src/components/Login.js
+++ web/src/components/Login.js
@@ -2,11 +2,42 @@
 import React, { useState } from "react";
 
 const Login = () => {
+
+  var loginName;
+  var loginPassword;
+  
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleLogin = () => {
+  const handleLogin = async (event) => {
     // Handle login logic here
+    var obj = {login:loginName.value,password:loginPassword.value};
+    var js = JSON.stringify(obj);
+
+    try
+    {
+      const response = await fetch('http://localhost:5000/api/login',
+        {method:'POST',body:js,headers:{'Content-Type':'application/json'}});
+
+      var res = JSON.parse(await response.text());
+      
+      if( res.id <= 0 )
+      {
+        setMessage('User/Password combination incorrect');
+      }
+      else
+      {
+       var user = {firstName:res.firstName,lastName:res.lastName,id:res.id}
+       localStorage.setItem('user_data', JSON.stringify(user));
+       setMessage('');
+       window.location.href = '/cards';
+      }
+    }
+    catch(e)
+    {
+      alert(e.toString());
+      return;
+    }
   };
 
   return (
