892ce6da2190a722551c895ccac107abb01c44ef
ashawaters <55795969+ashawaters@users.noreply.github.com>
2023-10-26 18:02:45 -0400

    update files
    updated login and register page

---
+++ web/src/pages/RegisterPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import PageTitle from '../components/PageTitle';
+import Register from '../components/Register';
+
+const RegisterPage = () =>
+{
+    return(
+        <div>
+            <PageTitle />
+            <Login />
+        </div>
+    );
+};
+
+export default RegisterPage;
