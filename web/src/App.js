24f97910afe43339446b05f60121b9c198c6a35d
tylerphungg <142745302+tylerphungg@users.noreply.github.com>
2023-10-30 15:25:00 -0400

    Update App.js

--- web/src/App.js
+++ web/src/App.js
@@ -3,15 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 
 import LoginPage from './pages/LoginPage';
-import CardPage from './pages/CardPage';
 import LandingPage from './pages/LandingPage';
 
-function App()
-{
+function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} /> {/* Add a route for the login page */}
       </Routes>
     </BrowserRouter>
   );
