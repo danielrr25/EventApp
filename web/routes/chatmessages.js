9ac313a5aad91eddd9896016999990c497363e60
stevenc15 <143041565+stevenc15@users.noreply.github.com>
2023-11-01 15:25:23 -0400

    Updated chatmessages.js  with add message route

--- web/routes/chatmessages.js
+++ web/routes/chatmessages.js
@@ -4,14 +4,13 @@ const express = require('express');
 const router = express.Router();
 const ChatMessage = require('../models/chatMessageSchema'); // Import the ChatMessage model
 
-// Define your routes and endpoints here
-
-// Example route for creating a chat message
-router.post('/create', async (req, res) => {
+// add chat message route
+router.post('/addchatmessage', async (req, res) => {
   try {
-    const chatMessage = new ChatMessage(req.body); // Create a new chat message from the request body
-    const savedChatMessage = await chatMessage.save(); // Save the chat message to the database
-    res.json(savedChatMessage);
+    const {EventId, SenderId, Timestamp, body} = req.body; 
+    const chatmessage = new ChatMessage({ EventId, SenderId, Timestamp, body });
+    await chatmessage.save();
+    res.status(201).send({ message: 'message added successfully' });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
