507d53678d57fa402a43ff1b701d6e51d34e07a4
stevenc15 <143041565+stevenc15@users.noreply.github.com>
2023-11-01 15:30:13 -0400

    Updated users.js with delete, add/remove friend routes

--- web/routes/users.js
+++ web/routes/users.js
@@ -74,4 +74,74 @@ router.get('/user-info/:id', async (req, res) => {
     }
   });
 
+// delete user route
+router.post('/deleteuser', async (req, res) => {
+    try {
+        const { username } = req.body;
+        const existingUser = await User.findOneAndRemove({ username });
+
+        if (!existingUser) {
+            return res.status(400).send({ error: 'User does not exist' });
+        }        
+        
+        res.status(201).send({ message: 'User deleted successfully' });
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+//add friend route
+router.post('/addfriend', async (req, res) => {
+    try {
+        const { myUsername, friendUsername } = req.body;
+        const user = await User.findOne({ username: myUsername });
+        const friend = await User.findOne({ username: friendUsername });
+
+        if (!user) {
+            return res.status(400).send({ error: 'Invalid user' });
+        }
+
+        if (!friend) {
+            return res.status(404).send({ error: 'Friend does not exist' });
+        }
+
+        if (user.friendslist.includes(friend._id)){
+            return res.status(408).send({ error: 'Friend already added' });
+        }
+
+        user.friendslist.push(friend._id);
+        await user.save();
+        res.status(201).send({ message: 'Friend added successfully' });
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+//remove friend route
+router.post('/removefriend', async (req, res) => {
+    try {
+        const { myUsername, friendUsername } = req.body;
+        const user = await User.findOne({ username: myUsername });
+        const friend = await User.findOne({ username: friendUsername });
+
+        if (!user) {
+            return res.status(400).send({ error: 'Invalid user' });
+        }
+
+        if (!friend) {
+            return res.status(404).send({ error: 'Friend does not exist' });
+        }
+
+        if (!user.friendslist.includes(friend._id)){
+            return res.status(408).send({ error: 'Friend not in list' });
+        }
+
+        user.friendslist = user.friendslist.filter(id => id.toString() !== friend._id.toString());
+        await user.save();
+        res.status(201).send({ message: 'Friend removed successfully' });
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
 module.exports = router;
