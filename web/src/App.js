c0e7415764fa562bc952a773ee60ce4e3ef18895
ashawaters <55795969+ashawaters@users.noreply.github.com>
2023-11-05 16:54:49 -0500

    Updated & added new files for events
    created Event.js, EventPage.js and modified App.js for events

--- web/src/App.js
+++ web/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import LoginPage from './pages/LoginPage';
 import LandingPage from './pages/LandingPage';
 import RegisterPage from './pages/RegisterPage'
+import EventPage from './pages/EventPage'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} /> {/* Add a route for the login page */}
         <Route path="/register" element={<RegisterPage />} /> {/* Add a route for the registration page */}
+        <Route path="/event" element={<EventPage />} /> {/* Add a route for the event page */}
       </Routes>
     </BrowserRouter>
   );
---
+++ web/src/components/Event.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Event() {
+  return <h1 id="title">Event Page</h1>;
+}
+export default Event;
