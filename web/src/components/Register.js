ccd4f1da2a134264394dd239f6016920f84802cd
tylerphungg <142745302+tylerphungg@users.noreply.github.com>
2023-11-05 23:19:52 -0500

    Update Register.js

--- web/src/components/Register.js
+++ web/src/components/Register.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import './Register.css'; // Import your CSS file
+import './Register.css';
 import standard_logo2 from './PopOutGradient.png';
-import { Link, useNavigate } from 'react-router-dom'; // Import Link
+import { Link, useNavigate } from 'react-router-dom';
 
 const Register = () => {
   const [username, setUsername] = useState("");
@@ -12,68 +12,56 @@ const Register = () => {
   const [message, setMessage] = useState('');
   const navigate = useNavigate();
 
-  const handleRegister = async () => {
+  const handleRegister = async (event) => {
+    event.preventDefault();
 
-    //verify all fields completed
-    if(!username || !password || !firstName || !lastName || !email){
-      setMessage('All fields required to register.')
+    if (!username || !password || !firstname || !lastname || !email) {
+      await setMessage('All fields are required to register.');
+      console.log('Message:', message);
       return;
     }
-    
-    // Handle registration logic here
+
     try {
-        // Create a data object with the user's email and password
-        const data = {
-          username,
-          password,
-          firstname,
-          lastname,
-          email,
-        };
-  
-        // Send a POST request to registration API endpoint
-        const response = await fetch('http://167.172.230.181:5000/users/register', {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(data),
-        });
-  
-        if (response.ok) {
-          // Registration successful, you can redirect the user or show a success message
-          setMessage('Registration successful!');
-          navigate('/event'); //redirect to event page
-        } else {
-          // Registration failed, handle errors
-          setMessage('Registration failed');
-        }
-      } catch (error) {
-        setMessage('An error occurred. Please try again.')
+      const data = {
+        username,
+        password,
+        firstname,
+        lastname,
+        email,
+      };
+
+      const response = await fetch('http://167.172.230.181:5000/users/register', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.status === 201) {
+        setMessage('Registration successful');
+        navigate('/login');
+      } else if (response.status === 400) {
+        await setMessage('Username already exists');
+        console.log('Message:', message);
+      } else if (response.status === 500) {
+        await setMessage('A server error has occurred.');
+      } else {
+        await setMessage('Unknown error');
       }
-
-
-        //verify registration information
-        if(response.status === 201){
-          setMessage('Registration successful')
-          navigate('/event');
-        } else if (response.status === 400){
-          setMessage('Username already exists');
-        } else if (response.status === 500){
-            setMessage('A server error has occurred.')
-        } else {
-          setMessage('Unknown error');
-        }
+    } catch (error) {
+      setMessage('An error occurred. Please try again.');
+    }
   };
 
   return (
     <div className="register-container">
       <h2 className="login-title2">WELCOME TO</h2>
-        <img src={standard_logo2} alt="Your Logo" className="standard_logo2" />
+      <img src={standard_logo2} alt="Your Logo" className="standard_logo2" />
       <h2 className="register-title">Register</h2>
-      <form className="register-form">
+      <form className="register-form" onSubmit={handleRegister}>
         <input
-          type="username"
+          type="text"
           className="register-input"
           placeholder="Username"
           value={username}
@@ -86,7 +74,6 @@ const Register = () => {
           value={firstname}
           onChange={(e) => setFirstName(e.target.value)}
         />
-
         <input
           type="text"
           className="register-input"
@@ -108,8 +95,10 @@ const Register = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button className="register-button" onClick={handleRegister}>Register</button>
+        <button type="submit" className="register-button">Register</button>
       </form>
+       {message && <p className="message">{message}</p>}
+
     </div>
   );
 };
