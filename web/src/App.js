16780a966b020530d1c5309c8db6d1ef4c4b5271
tylerphungg <142745302+tylerphungg@users.noreply.github.com>
2023-11-10 00:11:34 -0500

    Update App.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import LoginPage from './pages/LoginPage';
@@ -9,17 +9,21 @@ import AddEventPage from './pages/AddEventPage';
 import AccountPage from './pages/AccountPage';
 import EventPage from './pages/EventPage';
 
+import { UserProvider } from './components/UserContext'; // Import the UserProvider
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} /> 
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/addevent" element={<AddEventPage />} />
-        <Route path="/account" element={<AccountPage />} />
-        <Route path="/event" element={<EventPage />} />
-      </Routes>
+      <UserProvider> {/* Wrap your entire app with UserProvider */}
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/addevent" element={<AddEventPage />} />
+          <Route path="/account" element={<AccountPage />} />
+          <Route path="/event" element={<EventPage />} />
+        </Routes>
+      </UserProvider>
     </BrowserRouter>
   );
 }
