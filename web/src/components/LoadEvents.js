e7ddaaf7c5eff55b133d026fc67db555de1718b5
tylerphungg <142745302+tylerphungg@users.noreply.github.com>
2023-11-09 20:25:38 -0500

    Create LoadEvents.js

---
+++ web/src/components/LoadEvents.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+
+
+const getIconPath = (iconName) => {
+  try {
+    const icon = require(`./${iconName}.png`);
+    return icon;
+  } catch (error) {
+    console.error(`Error loading icon "${iconName}":`, error.message);
+    return './icon1.png';
+  }
+};
+
+function LoadEvents() {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const response = await fetch('http://167.172.230.181:5000/events/search-events');
+        const eventsData = await response.json();
+        setEvents(eventsData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchEvents();
+  }, []);
+
+  return (
+    <div id="eventUIDiv">
+      <div className="header-container">
+        <div className="title">
+          <h1>Event List</h1>
+        </div>
+      </div>
+
+      <div id="events-container" className="input-group">
+        {events.map(event => (
+          <div key={event._id} className="event">
+            <strong>Name:</strong> {event.eventName}<br />
+            <strong>Date:</strong> {event.eventDate}<br />
+            <strong>Category:</strong> {event.eventCategory}<br />
+            <strong>Location:</strong> {event.eventLocation}<br />
+            {event.eventIcon && (
+              <div>
+                <strong>Icon:</strong>
+                <img src={getIconPath(event.eventIcon)} alt="Event Icon" />
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default LoadEvents;
