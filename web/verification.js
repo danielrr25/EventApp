247492840c82e05708be3e3e0a43844ad1cd9cbf
stevenc15 <143041565+stevenc15@users.noreply.github.com>
2023-11-12 21:45:25 -0500

    Update and rename everification.js to verification.js

--- web/verification.js
+++ web/verification.js
@@ -1,10 +1,10 @@
 const { v4: uuidv4 } = require('uuid');
 
-const genemailtoken = () => {
+const gentoken = () => {
     const emailToken = uuidv4();
     return emailToken;
 };
 
 module.exports = {
-    genemailtoken,
+    gentoken,
 };
