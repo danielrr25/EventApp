0a4330e934c23c6d22ba2494436a29d37c209666
Ceraphims <91357886+chrisfan2@users.noreply.github.com>
2023-11-13 21:14:48 -0500

    login validation + reset pass page design changes

--- mobile/lib/forgot_password.dart
+++ mobile/lib/forgot_password.dart
@@ -11,8 +11,11 @@ class ResetPassword extends StatefulWidget {
 }
 
 class _ResetPasswordState extends State<ResetPassword> {
+  final _formfield = GlobalKey<FormState>();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passCodeController = TextEditingController();
+  bool passToggle = true;
+
   String errorMessage = "";
   @override
   Widget build(BuildContext context) {
@@ -21,110 +24,114 @@ class _ResetPasswordState extends State<ResetPassword> {
         body: SafeArea(
           child: SingleChildScrollView(
             physics: const BouncingScrollPhysics(),
-            child: Column(children: [
-              Image.asset(
-                'assets/icon/no_background_logo.jpg',
-                height: 160,
-                width: 160,
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                "Reset Password",
-                style: TextStyle(
-                  color: Color.fromARGB(255, 0, 0, 0),
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
+            child: Form(
+              key: _formfield,
+              child: Column(children: [
+                Image.asset(
+                  'assets/icon/no_background_logo.jpg',
+                  height: 160,
+                  width: 160,
                 ),
-              ),
-              const Text(
-                "Enter Your Email",
-                style: TextStyle(
-                  color: Color.fromARGB(255, 0, 0, 0),
-                  fontSize: 20,
+                const SizedBox(height: 20),
+                const Text(
+                  "Reset Password",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              Text(errorMessage,
-                  style:
-                      const TextStyle(color: Color.fromARGB(255, 243, 4, 4))),
-              const SizedBox(height: 15),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 23.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: const Text(
-                        "Return to Login",
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 29, 71, 98),
-                          fontSize: 16,
+                const Text(
+                  "Enter Your Email",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontSize: 20,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(errorMessage,
+                    style:
+                        const TextStyle(color: Color.fromARGB(255, 243, 4, 4))),
+                const SizedBox(height: 15),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 23.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text(
+                          "Return to Login",
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 29, 71, 98),
+                            fontSize: 16,
+                          ),
                         ),
                       ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                        labelText: "Email",
+                        prefixIcon: const Align(
+                          widthFactor: 1.0,
+                          heightFactor: 1.0,
+                          child: Icon(Icons.email_rounded),
+                        ),
+                        border: const OutlineInputBorder(),
+                        focusedBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue),
+                        ),
+                        fillColor: const Color.fromARGB(255, 252, 250, 250),
+                        filled: true,
+                        hintStyle: TextStyle(color: Colors.grey[500])),
+                  ),
+                ),
+                const SizedBox(height: 15),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    controller: _passCodeController,
+                    decoration: InputDecoration(
+                      labelText: "Passcode",
+                      prefixIcon: const Align(
+                        widthFactor: 1.0,
+                        heightFactor: 1.0,
+                        child: Icon(Icons.lock),
+                      ),
+                      border: const OutlineInputBorder(),
+                      focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue),
+                      ),
+                      fillColor: const Color.fromARGB(255, 252, 250, 250),
+                      filled: true,
+                      hintStyle: TextStyle(color: Colors.grey[500]),
                     ),
-                  ],
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: TextField(
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                      prefixIcon: const Align(
-                        widthFactor: 1.0,
-                        heightFactor: 1.0,
-                        child: Icon(Icons.email_rounded),
-                      ),
-                      hintText: 'Email',
-                      enabledBorder: const OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white),
-                      ),
-                      focusedBorder: const OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.blue),
-                      ),
-                      fillColor: const Color.fromARGB(255, 252, 250, 250),
-                      filled: true,
-                      hintStyle: TextStyle(color: Colors.grey[500])),
+                const SizedBox(height: 25),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      foregroundColor: Colors.white,
+                      minimumSize: const Size(365, 50),
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(8)))),
+                  onPressed: () {},
+                  child: const Text('Reset Password',
+                      style: TextStyle(fontSize: 20)),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: TextField(
-                  controller: _passCodeController,
-                  decoration: InputDecoration(
-                      prefixIcon: const Align(
-                        widthFactor: 1.0,
-                        heightFactor: 1.0,
-                        child: Icon(Icons.email_rounded),
-                      ),
-                      hintText: 'Enter PassCode',
-                      enabledBorder: const OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white),
-                      ),
-                      focusedBorder: const OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.blue),
-                      ),
-                      fillColor: const Color.fromARGB(255, 252, 250, 250),
-                      filled: true,
-                      hintStyle: TextStyle(color: Colors.grey[500])),
-                ),
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.black,
-                    foregroundColor: Colors.white,
-                    minimumSize: const Size(365, 50),
-                    shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8)))),
-                onPressed: null,
-                child: const Text('Reset Password',
-                    style: TextStyle(fontSize: 20)),
-              ),
-              const SizedBox(height: 20),
-            ]),
+                const SizedBox(height: 20),
+              ]),
+            ),
           ),
         ));
   }
