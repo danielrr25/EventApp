b9799c1497125be606783a400935358f9f363916
oliverkristianfritsche <61664788+oliverkristianfritsche@users.noreply.github.com>
2023-11-15 00:39:01 -0500

    unique jwt errors

--- web/utils/jwt.js
+++ web/utils/jwt.js
@@ -6,13 +6,13 @@ const verifyToken = (req, res, next) => {
     if (token) {
       jwt.verify(token, JWT_SECRET, (err, decoded) => {
         if (err) {
-          return res.status(401).json({ error: 'Unauthorized access: Invalid token' });
+          return res.status(111).json({ error: 'Unauthorized access: Invalid token' });
         }
         req.user = decoded; // Add the decoded token to the request for use in your routes
         next();
       });
     } else {
-      res.status(403).json({ error: 'A token is required for authentication' });
+      res.status(222).json({ error: 'A token is required for authentication' });
     }
   };
 
