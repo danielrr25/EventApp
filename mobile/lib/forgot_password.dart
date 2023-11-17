8b86db98158646fa361c65fec7a7b793aa293d10
Ceraphims <91357886+chrisfan2@users.noreply.github.com>
2023-11-16 23:24:32 -0500

    Implemented password reset. Also Added validation for password complexity when password is changed. Passwords are required to have upper and lowercase letters, a number, and a special symbol

--- mobile/lib/forgot_password.dart
+++ mobile/lib/forgot_password.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/new_password.dart';
-// import 'package:http/http.dart' as http;
-// import 'dart:convert';
-// import 'package:mobile/components/text_field.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class ResetPassword extends StatefulWidget {
   const ResetPassword({super.key});
@@ -15,9 +14,98 @@ class _ResetPasswordState extends State<ResetPassword> {
   final _formfield = GlobalKey<FormState>();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passCodeController = TextEditingController();
-  bool passToggle = true;
-
+  final sendCodeUrl = "http://167.172.230.181:5000/users/resetpassword";
+  final verifyCodeUrl =
+      "http://167.172.230.181:5000/users/resetpasswordentercode";
   String errorMessage = "";
+
+  Future<int> sendEmailResetCode() async {
+    var response = await http.post(Uri.parse(sendCodeUrl),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, String>{
+          "email": _emailController.text,
+        }));
+
+    print("sendEmailResetCode Status Code: ${response.statusCode}");
+
+    if (response.statusCode == 201) {
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text("Reset Code Sent to Email"),
+          duration: Duration(milliseconds: 1500),
+        ));
+      }
+    }
+
+    if (response.statusCode == 404) {
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text("No Account Associated With This Email."),
+          duration: Duration(milliseconds: 1500),
+        ));
+      }
+    }
+
+    if (response.statusCode == 500) {
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text("Server Error. Try Again Later"),
+          duration: Duration(milliseconds: 1500),
+        ));
+      }
+    }
+
+    return response.statusCode;
+  }
+
+  Future<int> verifyResetCode() async {
+    var response = await http.post(Uri.parse(verifyCodeUrl),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, String>{
+          "code": _passCodeController.text,
+        }));
+
+    if (response.statusCode == 201) {
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text("Password Successfully Reset"),
+          duration: Duration(milliseconds: 1500),
+        ));
+      }
+    }
+
+    if (response.statusCode == 404) {
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content:
+              Text("Invalid Code. Please Type In The Code Sent to Your Email"),
+          duration: Duration(milliseconds: 1500),
+        ));
+      }
+    }
+
+    if (response.statusCode == 500) {
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text("Server Error. Try Again Later"),
+          duration: Duration(milliseconds: 1500),
+        ));
+      }
+    }
+
+    return response.statusCode;
+  }
+
+  bool emailValidator(email) {
+    return RegExp(
+            r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+        .hasMatch(email);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -84,44 +172,76 @@ class _ResetPasswordState extends State<ResetPassword> {
                     Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 25.0),
                       child: TextFormField(
-                        keyboardType: TextInputType.emailAddress,
-                        controller: _emailController,
-                        decoration: InputDecoration(
-                            labelText: "Email",
-                            prefixIcon: const Align(
-                              widthFactor: 1.0,
-                              heightFactor: 1.0,
-                              child: Icon(Icons.email_rounded),
-                            ),
-                            border: const OutlineInputBorder(),
-                            focusedBorder: const OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.blue),
-                            ),
-                            fillColor: const Color.fromARGB(255, 252, 250, 250),
-                            filled: true,
-                            hintStyle: TextStyle(color: Colors.grey[500]),
-                            suffixIcon: TextButton(
-                              onPressed: () {},
-                              style: TextButton.styleFrom(
-                                  minimumSize: Size.zero,
-                                  padding: const EdgeInsets.only(right: 10),
-                                  tapTargetSize:
-                                      MaterialTapTargetSize.shrinkWrap),
-                              child: const Text(
-                                'Send Code',
-                                style: TextStyle(
-                                  color: Color.fromARGB(255, 25, 90, 131),
-                                  fontSize: 16,
-                                ),
+                          keyboardType: TextInputType.emailAddress,
+                          controller: _emailController,
+                          decoration: InputDecoration(
+                              labelText: "Email",
+                              prefixIcon: const Align(
+                                widthFactor: 1.0,
+                                heightFactor: 1.0,
+                                child: Icon(Icons.email_rounded),
                               ),
-                            )),
-                      ),
+                              border: const OutlineInputBorder(),
+                              focusedBorder: const OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.blue),
+                              ),
+                              fillColor:
+                                  const Color.fromARGB(255, 252, 250, 250),
+                              filled: true,
+                              hintStyle: TextStyle(color: Colors.grey[500]),
+                              suffixIcon: TextButton(
+                                onPressed: () async {
+                                  setState(() {
+                                    errorMessage = '';
+                                  });
+                                  if (_formfield.currentState!.validate()) {
+                                    int statusCode = await sendEmailResetCode();
+
+                                    if (statusCode == 404) {
+                                      setState(() {
+                                        errorMessage =
+                                            'No Account Associated With This Email';
+                                      });
+                                    }
+
+                                    if (statusCode == 500) {
+                                      setState(() {
+                                        errorMessage =
+                                            'Could Not Send Email. Internal Server Error. Try Again Later';
+                                      });
+                                    }
+                                  }
+                                },
+                                style: TextButton.styleFrom(
+                                    minimumSize: Size.zero,
+                                    padding: const EdgeInsets.only(right: 10),
+                                    tapTargetSize:
+                                        MaterialTapTargetSize.shrinkWrap),
+                                child: const Text(
+                                  'Send Code',
+                                  style: TextStyle(
+                                    color: Color.fromARGB(255, 25, 90, 131),
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              )),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Enter Email";
+                            }
+
+                            if (emailValidator(value) == false) {
+                              return "Enter Valid Email Address";
+                            }
+
+                            return null;
+                          }),
                     ),
                     const SizedBox(height: 15),
                     Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 25.0),
                       child: TextFormField(
-                        keyboardType: TextInputType.number,
+                        keyboardType: TextInputType.emailAddress,
                         controller: _passCodeController,
                         decoration: InputDecoration(
                           labelText: "Passcode",
@@ -149,11 +269,35 @@ class _ResetPasswordState extends State<ResetPassword> {
                           shape: const RoundedRectangleBorder(
                               borderRadius:
                                   BorderRadius.all(Radius.circular(8)))),
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const NewPassword()));
+                      onPressed: () async {
+                        setState(() {
+                          errorMessage = '';
+                        });
+                        int statusCode = await verifyResetCode();
+
+                        print(statusCode);
+
+                        if (statusCode == 201) {
+                          if (context.mounted) {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const NewPassword()));
+                          }
+                        }
+
+                        if (statusCode == 404) {
+                          setState(() {
+                            errorMessage = 'Incorrect Code';
+                          });
+                        }
+
+                        if (statusCode == 500) {
+                          setState(() {
+                            errorMessage =
+                                'Internal Server Error. Try Again Later';
+                          });
+                        }
                       },
                       child: const Text('Reset Password',
                           style: TextStyle(fontSize: 20)),
--- mobile/lib/new_password.dart
+++ mobile/lib/new_password.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/loginpage.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class NewPassword extends StatefulWidget {
   const NewPassword({super.key});
@@ -10,11 +12,42 @@ class NewPassword extends StatefulWidget {
 
 class _NewPasswordState extends State<NewPassword> {
   final _formfield = GlobalKey<FormState>();
+  final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _confirmPasswordController =
       TextEditingController();
-
+  final url = "http://167.172.230.181:5000/users/changepassword";
   String errorMessage = "";
+  bool passToggleNewPass = true;
+  bool passToggleConfirmPass = true;
+
+  Future<int> verifyChangePassword() async {
+    var response = await http.post(Uri.parse(url),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, String>{
+          "username": _usernameController.text,
+          "newpassword": _passwordController.text,
+          "confirmpassword": _confirmPasswordController.text,
+        }));
+
+    if (response.statusCode == 201) {
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text("Password Changed Successfully"),
+          duration: Duration(milliseconds: 1500),
+        ));
+      }
+    }
+
+    return response.statusCode;
+  }
+
+  bool passwordValidator(password) {
+    return RegExp(r"(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)").hasMatch(password);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,47 +89,115 @@ class _NewPasswordState extends State<NewPassword> {
                     Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 25.0),
                       child: TextFormField(
-                        keyboardType: TextInputType.emailAddress,
-                        controller: _passwordController,
-                        decoration: InputDecoration(
-                          labelText: "New Password",
-                          prefixIcon: const Align(
-                            widthFactor: 1.0,
-                            heightFactor: 1.0,
-                            child: Icon(Icons.lock_outline_rounded),
+                          keyboardType: TextInputType.emailAddress,
+                          controller: _usernameController,
+                          decoration: InputDecoration(
+                            labelText: "Enter Account Username",
+                            prefixIcon: const Align(
+                              widthFactor: 1.0,
+                              heightFactor: 1.0,
+                              child: Icon(Icons.lock_outline_rounded),
+                            ),
+                            border: const OutlineInputBorder(),
+                            focusedBorder: const OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.blue),
+                            ),
+                            fillColor: const Color.fromARGB(255, 252, 250, 250),
+                            filled: true,
+                            hintStyle: TextStyle(color: Colors.grey[500]),
                           ),
-                          border: const OutlineInputBorder(),
-                          focusedBorder: const OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.blue),
-                          ),
-                          fillColor: const Color.fromARGB(255, 252, 250, 250),
-                          filled: true,
-                          hintStyle: TextStyle(color: Colors.grey[500]),
-                        ),
-                      ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Enter Account Username";
+                            }
+
+                            return null;
+                          }),
                     ),
                     const SizedBox(height: 15),
                     Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 25.0),
                       child: TextFormField(
-                        keyboardType: TextInputType.number,
-                        controller: _confirmPasswordController,
-                        decoration: InputDecoration(
-                          labelText: "Confirm New Password",
-                          prefixIcon: const Align(
-                            widthFactor: 1.0,
-                            heightFactor: 1.0,
-                            child: Icon(Icons.lock),
+                          keyboardType: TextInputType.emailAddress,
+                          controller: _passwordController,
+                          obscureText: passToggleNewPass,
+                          decoration: InputDecoration(
+                            labelText: "New Password",
+                            prefixIcon: const Align(
+                              widthFactor: 1.0,
+                              heightFactor: 1.0,
+                              child: Icon(Icons.lock_outline_rounded),
+                            ),
+                            border: const OutlineInputBorder(),
+                            focusedBorder: const OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.blue),
+                            ),
+                            fillColor: const Color.fromARGB(255, 252, 250, 250),
+                            filled: true,
+                            hintStyle: TextStyle(color: Colors.grey[500]),
+                            errorMaxLines: 2,
+                            suffixIcon: InkWell(
+                              onTap: () {
+                                setState(() {
+                                  passToggleNewPass = !passToggleNewPass;
+                                });
+                              },
+                              child: Icon(passToggleNewPass
+                                  ? Icons.visibility
+                                  : Icons.visibility_off),
+                            ),
                           ),
-                          border: const OutlineInputBorder(),
-                          focusedBorder: const OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.blue),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Enter New Password";
+                            }
+                            if (passwordValidator(value) == false) {
+                              return "Password Must Have a Capital and Lowercase letter, a Number, and a Special Symbol (Like !, #, @, etc.)";
+                            }
+
+                            return null;
+                          }),
+                    ),
+                    const SizedBox(height: 15),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          controller: _confirmPasswordController,
+                          obscureText: passToggleConfirmPass,
+                          decoration: InputDecoration(
+                            labelText: "Confirm New Password",
+                            prefixIcon: const Align(
+                              widthFactor: 1.0,
+                              heightFactor: 1.0,
+                              child: Icon(Icons.lock),
+                            ),
+                            border: const OutlineInputBorder(),
+                            focusedBorder: const OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.blue),
+                            ),
+                            fillColor: const Color.fromARGB(255, 252, 250, 250),
+                            filled: true,
+                            hintStyle: TextStyle(color: Colors.grey[500]),
+                            suffixIcon: InkWell(
+                              onTap: () {
+                                setState(() {
+                                  passToggleConfirmPass =
+                                      !passToggleConfirmPass;
+                                });
+                              },
+                              child: Icon(passToggleConfirmPass
+                                  ? Icons.visibility
+                                  : Icons.visibility_off),
+                            ),
                           ),
-                          fillColor: const Color.fromARGB(255, 252, 250, 250),
-                          filled: true,
-                          hintStyle: TextStyle(color: Colors.grey[500]),
-                        ),
-                      ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Confirm New Password";
+                            }
+
+                            return null;
+                          }),
                     ),
                     const SizedBox(height: 25),
                     ElevatedButton(
@@ -107,13 +208,47 @@ class _NewPasswordState extends State<NewPassword> {
                           shape: const RoundedRectangleBorder(
                               borderRadius:
                                   BorderRadius.all(Radius.circular(8)))),
-                      onPressed: () {
-                        Navigator.pushAndRemoveUntil(context,
-                            MaterialPageRoute(builder: (BuildContext context) {
-                          return const LoginPage();
-                        }), (r) {
-                          return false;
+                      onPressed: () async {
+                        setState(() {
+                          errorMessage = '';
                         });
+
+                        if (_formfield.currentState!.validate()) {
+                          int statusCode = await verifyChangePassword();
+
+                          print("STATUS CODE IN CHANGE PASS: ${statusCode}");
+
+                          if (statusCode == 201) {
+                            if (context.mounted) {
+                              Navigator.pushAndRemoveUntil(context,
+                                  MaterialPageRoute(
+                                      builder: (BuildContext context) {
+                                return const LoginPage();
+                              }), (r) {
+                                return false;
+                              });
+                            }
+                          }
+
+                          if (statusCode == 400) {
+                            setState(() {
+                              errorMessage = 'Passwords Do Not Match';
+                            });
+                          }
+
+                          if (statusCode == 404) {
+                            setState(() {
+                              errorMessage = "User Does Not Exist";
+                            });
+                          }
+
+                          if (statusCode == 500) {
+                            setState(() {
+                              errorMessage =
+                                  'Change Password Failed. Internal Server Error. Try Again Later';
+                            });
+                          }
+                        }
                       },
                       child: const Text('Change Password',
                           style: TextStyle(fontSize: 20)),
