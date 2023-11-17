35bdf61fbbf6643350d23cde8ff08f5224f5493e
oliverkristianfritsche <61664788+oliverkristianfritsche@users.noreply.github.com>
2023-11-17 16:36:02 -0500

    JWT stuff polished

--- web/utils/jwt.js
+++ web/utils/jwt.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const JWT_SECRET = "TheIndustrialRevolutionAndItsConsequencesHaveBeenADisasterForTheHumanRace";//change this to an evniroment variable later
 const verifyToken = (req, res, next) => {
-    const token = req.headers['authorization']?.split(' ')[1]; // Bearer <token>
+    const token = req.headers['authorization']; // Bearer <token>
     if (token) {
       jwt.verify(token, JWT_SECRET, (err, decoded) => {
         if (err) {
