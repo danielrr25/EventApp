b98b84b53efba2326f84e63a618546b0fd8d7cd7
Ceraphims <91357886+chrisfan2@users.noreply.github.com>
2023-11-20 04:40:12 -0500

    Added popup confirmation for Logout and DeleteAccount() and fixed overflow in displaying user info in user section

--- mobile/lib/sections/user_section.dart
+++ mobile/lib/sections/user_section.dart
@@ -41,20 +41,95 @@ class _UserSettingsState extends State<UserSettings> {
     }
   }
 
+  Future<void> showLogoutConfirmationPopUp() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Logout Confirmation'),
+          content: const SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                    "You will be logged out of your account. You will need to sign back in if you want to access your events.\n"),
+                Text("Are you sure?"),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Log out'),
+              onPressed: () {
+                print('Confirmed');
+                Navigator.of(context).pop();
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> showDeleteAccountConfirmationPopUp() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Delete Account Confirmation'),
+          content: const SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                    "You will permanently delete your account and will lose all access to all your events.\n"),
+                Text("Are you sure you want to delete your account?"),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Yes, Delete My Account'),
+              onPressed: () {
+                print('Confirmed');
+                Navigator.of(context).pop();
+                deleteAccount();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
+      body: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                  'Name: ${currentUser.firstname} ${currentUser.lastname} \nUsername: ${currentUser.username} \nEmail: ${currentUser.email}',
-                  style: const TextStyle(
-                      fontSize: 30, fontWeight: FontWeight.bold)),
-            ],
+          child: FittedBox(
+            child: Text(
+              'Name: ${currentUser.firstname} ${currentUser.lastname} \nUsername: ${currentUser.username} \nEmail: ${currentUser.email}',
+              style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
           ),
         ),
         const SizedBox(height: 20),
@@ -66,7 +141,7 @@ class _UserSettingsState extends State<UserSettings> {
               shape: const RoundedRectangleBorder(
                   borderRadius: BorderRadius.all(Radius.circular(8)))),
           onPressed: () {
-            deleteAccount();
+            showDeleteAccountConfirmationPopUp();
           },
           child: const Text('Delete Account', style: TextStyle(fontSize: 20)),
         ),
@@ -79,7 +154,7 @@ class _UserSettingsState extends State<UserSettings> {
               shape: const RoundedRectangleBorder(
                   borderRadius: BorderRadius.all(Radius.circular(8)))),
           onPressed: () {
-            Navigator.pop(context);
+            showLogoutConfirmationPopUp();
           },
           child: const Text('Log out', style: TextStyle(fontSize: 20)),
         ),
